3202d9077d956dfbfe35e8657ac5340ab5515273
Marc Teale <marc.teale@allstardirectories.com>
2016-04-26 08:56:33 -0700

    templates/module.erb

--- manifests/server/module.pp
+++ manifests/server/module.pp
@@ -19,12 +19,13 @@
 #   $auth_users       - list of usernames that will be allowed to connect to this module (must be undef or an array)
 #   $hosts_allow      - list of patterns allowed to connect to this module (man 5 rsyncd.conf for details, must be undef or an array)
 #   $hosts_deny       - list of patterns allowed to connect to this module (man 5 rsyncd.conf for details, must be undef or an array)
-#   $transfer_logging - parameter enables per-file logging of downloads and 
+#   $transfer_logging - parameter enables per-file logging of downloads and
 #    uploads in a format somewhat similar to that used by ftp daemons.
-#   $log_format       - This parameter allows you to specify the format used 
-#    for logging file transfers when transfer logging is enabled. See the 
+#   $log_format       - This parameter allows you to specify the format used
+#    for logging file transfers when transfer logging is enabled. See the
 #    rsyncd.conf documentation for more details.
-#   $refuse_options   - list of rsync command line options that will be refused by your rsync daemon.
+#   $refuse_options   - list of rsync command line options that will be refused by your rsync daemon
+#   $numeric_ids      - yes||no, don’t map uid/gid values by user/group name, defaults to no
 #
 #   sets up an rsync server
 #
@@ -58,7 +59,8 @@ define rsync::server::module (
   $hosts_deny       = undef,
   $transfer_logging = undef,
   $log_format       = undef,
-  $refuse_options   = undef)  {
+  $refuse_options   = undef,
+  $numeric_ids      = undef)  {
 
   concat::fragment { "frag-${name}":
     content => template('rsync/module.erb'),
